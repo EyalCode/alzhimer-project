f82761a3f0c16a6cfcd82b9f678990510f664bb9
EyalCode <Eyalcha@gmail.com>
2025-11-28 10:30:48 +0200

    Fix for Pointnet++ impl

--- src/model.py
+++ src/model.py
@@ -128,7 +128,7 @@ class PointNetPlusPlus(nn.Module):
         self.normal_channel = normal_channel
         self.sa1 = PointNetSetAbstractionMsg(512, [0.1, 0.2, 0.4], [16, 32, 128], in_channel,[[32, 32, 64], [64, 64, 128], [64, 96, 128]])
         self.sa2 = PointNetSetAbstractionMsg(128, [0.2, 0.4, 0.8], [32, 64, 128], 320,[[64, 64, 128], [128, 128, 256], [128, 128, 256]])
-        self.sa3 = PointNetSetAbstraction(None, None, None, 640 + 2, [256, 512, 1024], True)
+        self.sa3 = PointNetSetAbstraction(None, None, None, 640, [256, 512, 1024], True)
         self.fc1 = nn.Linear(1024, 512)
         self.bn1 = nn.BatchNorm1d(512)
         self.drop1 = nn.Dropout(0.4)
